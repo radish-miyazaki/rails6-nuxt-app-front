83198fa629571e50afd726839f9b7c891a0600ca
radish-miyazaki <y.hidaka.kobe@gmail.com>
2020-11-13 18:02:17 +0900

    implemnt axios

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,35 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        app
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div>
+    <button
+      type="button"
+      name="button"
+      @click="getMsg"
+    >
+      RailsからAPIを取得する
+    </button>
+    <div
+      v-for="(msg i) in msgs"
+      :key="i"
+    >
+      {{ msg }}
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      msg: []
+    }
+  },
+  methods: {
+    getMsg() {
+      this.$axios.$get('/api/v1/hello')
+        .then(res => this.msgs.push(res))
+    }
+  }
+}
 </script>
 
 <style>
