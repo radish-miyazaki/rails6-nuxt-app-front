59235e27c09513d5986693cb9acbae834f0b1257
radish-miyazaki <y.hidaka.kobe@gmail.com>
2020-11-18 11:34:36 +0900

    add account menu pages and logout page

---
+++ pages/account.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <nuxt-child />
+  </v-container>
+</template>
+
+<script>
+export default {
+  layout: 'loggedIn',
+
+  // 自動生成される/accountパスを無効化
+  validate({ route }) {
+    return route.name !== 'account'
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/logout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+
+  // トップページ以外のページではこちらの処理でリダイレクト
+  async middleware({ store, redirect, from }) {
+    await store.dispatch('logout')
+    if (from.name !== 'index') { return redirect('/') }
+  },
+
+  // トップページの場合、こちらの処理でリダイレクト
+  layout: 'logout',
+  beforeCreate() {
+    this.$router.replace('/')
+  }
+}
+
+</script>
+
