526863eedbacbc1a0d074700a804053c4402d1b3
radish-miyazaki <y.hidaka.kobe@gmail.com>
2020-11-18 10:54:16 +0900

    add loggedIn toolbar

--- pages/index.vue
+++ pages/index.vue
@@ -8,8 +8,8 @@
 export default {
 
   layout({ store }) {
-    // ログインしている場合'default'を、ログインしていない場合'welcome'を返す
-    return store.state.loggedIn ? 'default' : 'welcome'
+    // ログインしている場合'loggedIn'を、ログインしていない場合'welcome'を返す
+    return store.state.loggedIn ? 'loggedIn' : 'welcome'
   }
   
 }
